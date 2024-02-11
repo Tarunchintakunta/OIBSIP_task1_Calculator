01545e2c327adc144e3dbb144393d15224688eb1
Tarunchintakunta <100082621+Tarunchintakunta@users.noreply.github.com>
2024-02-11 11:10:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button =>{
     button.addEventListener('click',(e) =>{
-        if(e.target.innerHTML == "="){
+        if(e.target.innerHTML == "=")
+        {
             string=eval(string);
             input.value=string;
         }
-        else if(e.target.innerHTML == 'AC'){
+        else if(e.target.innerHTML == 'AC')
+        {
             string="";
             input.value=string;
         }
-        else if (e.target.innerHTML== 'DEL'){
+        else if (e.target.innerHTML== 'DEL')
+        {
             string=string.substring(0,string.length-1);
             input.value = string;
         }
